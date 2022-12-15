9c4b1c14d4d6c2e60badaf3b2181dd8488d50569
ztNIE <niezhengtong@gmail.com>
2022-12-15 17:03:56 +1100

    Finish ps0.py

---
+++ ps0.py
@@ -0,0 +1,7 @@
+from math import log2
+
+if __name__ == "__main__":
+    x = int(input("Enter number x: "))
+    y = int(input("Enter number y: "))
+    print(f"x**y = {x**y}")
+    print(f"log(x) = {log2(x)}")
