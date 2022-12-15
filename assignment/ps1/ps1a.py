f27567b112aa9246bdd23bfd1044479fbf18d6bd
ztNIE <niezhengtong@gmail.com>
2022-12-15 17:21:17 +1100

    Finish ps1a.py

---
+++ assignment/ps1/ps1a.py
@@ -0,0 +1,20 @@
+portion_down_payment = 0.25
+r = 0.04
+current_saving = 0
+
+if __name__ == "__main__":
+    annual_salary = int(input("Enter your annual salary: "))
+    portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+    total_cost = int(input("Enter the cost of your dream home: "))
+
+    down_payment = total_cost * portion_down_payment
+    monthly_salary = annual_salary / 12
+
+    num_of_month = 0
+
+    while current_saving < down_payment:
+        current_saving += current_saving * r / 12 + portion_saved * monthly_salary
+        num_of_month += 1
+    
+    print(f"Number of months: {num_of_month}")
+    
